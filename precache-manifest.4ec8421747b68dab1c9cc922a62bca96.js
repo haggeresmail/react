dc9d4abac369923229da57552b22d76090f906da
hagger <haggersoma@gmail.com>
2023-12-07 16:51:59 +0200

    Updates

---
+++ precache-manifest.4ec8421747b68dab1c9cc922a62bca96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21757012a383c306443b4b302602e0e5",
+    "url": "/haggeresmail/react.git/index.html"
+  },
+  {
+    "revision": "2a152e997e5841ec4adf",
+    "url": "/haggeresmail/react.git/static/css/main.ca83a273.chunk.css"
+  },
+  {
+    "revision": "ddc2959ed5aac36722e1",
+    "url": "/haggeresmail/react.git/static/js/2.bc9fd01f.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/haggeresmail/react.git/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "2a152e997e5841ec4adf",
+    "url": "/haggeresmail/react.git/static/js/main.3bac4b34.chunk.js"
+  },
+  {
+    "revision": "b308dc584e906d300aa8",
+    "url": "/haggeresmail/react.git/static/js/runtime~main.9252d7fd.js"
+  }
+]);
